8967431a019ba3238713322029e34854e221899d
Kai <kwiddeson4@gmail.com>
2020-12-10 10:11:35 +0000

    add result outputting

--- MyCode/recommender.py
+++ MyCode/recommender.py
@@ -21,25 +21,65 @@ def make_comparable(items_row):
 def find_similarities(comparison_df):
     comparison_df = comparison_df.set_index("business_id")
     new_df = []
+    stored_indices = []
 
     for index, row in comparison_df.iterrows():
+        stored_indices.append(index)
         row = make_comparable(row)
         new_df.append(row)
 
-    return cosine_similarity(new_df)
+    return cosine_similarity(new_df), stored_indices
 
 
-def find_user_rated(user, rdf, bdf):
+def find_user_rated(user, rdf):
     user_reviews = rdf[rdf["user_id"] == user]
     return user_reviews
+
+
+def find_neighbours(matrix, reviewed, business_index, return_num):
+    id = reviewed["business_id"][0]
+    for i in range(len(business_index)):
+        if business_index[i] == id:
+            row_loc = i
+            break
+
+    sims_id = []
+    for i in range(len(matrix[row_loc])):
+        sims_id.append([matrix[row_loc][i], business_index[i]])
+
+    similarities = sorted(sims_id, reverse=True)
+
+    # Remove self from list of similarities
+    similarities = similarities[1:]
+
+    return similarities[:return_num]
+
+
+def display_results(results):
+    output = pd.DataFrame()
+    first = True
+    for item in results:
+        id = item[1]
+        result = businesses_df[businesses_df["business_id"] == id]
+        result["similarity"] = item[0]
+        if first:
+            output = result
+            first = False
+        else:
+            output = pd.concat([output, result])
+    print(output)
 
 
 # Eventually this should be changeable, this is just for testing purposes
 i_user_id = "2U2tqOCphgOQ-NX8b3P6nw"
 
-similarity_matrix = find_similarities(businesses_df)
-print(similarity_matrix)
+# find the similarity matrix between all businesses and store locations for later use
+similarity_matrix, indices = find_similarities(businesses_df)
 
-rated_items = find_user_rated(i_user_id, reviews_df, businesses_df)
-print(rated_items)
+# Find all businesses reviewed by the user
+rated_items = find_user_rated(i_user_id, reviews_df)
 
+# Find similar businesses to those which have been reviewed by user
+neighbours = find_neighbours(similarity_matrix, rated_items, indices, 12)
+
+display_results(neighbours)
