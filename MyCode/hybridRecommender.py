044036db8ea5e8b2bd85c0d983a91fe83c79cb39
Kai <kwiddeson4@gmail.com>
2021-01-30 11:42:14 +0000

    add second recommender to generate recommendations

--- MyCode/hybridRecommender.py
+++ MyCode/hybridRecommender.py
@@ -6,3 +6,4 @@ from recommenderTwo import *
 # Create user's recommendations using a hybrid scheme
 def generate_recommendations(user_id):
     recommender_one(user_id)
+    recommender_two(user_id)
