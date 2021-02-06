9bba92392b3fb7ecb31e289b2f42768157b54895
Kai <kwiddeson4@gmail.com>
2021-02-06 17:36:37 +0000

    Make a proper start on building CB recommender using sklearn

--- MyCode/contentBasedRecommender.py
+++ MyCode/contentBasedRecommender.py
@@ -1,43 +1,68 @@
-# This recommender uses content-based filtering
+# # This recommender uses content-based filtering
+#
+# # imports
+# import pandas as pd
+#
+#
+# def content_based_recommender(user_id):
+#     # Update the dataframes
+#     reviews_df = pd.read_csv("newDFReview.csv")
+#     users_df = pd.read_csv("newDFUser.csv")
+#     businesses_df = pd.read_csv("newDFBusiness.csv")
+#
+#     return temporary_recommender(businesses_df)
+#
+#
+# def temporary_recommender(businesses_df):
+#     rank = 1
+#     temp_rankings = []
+#     for index, row in businesses_df.iterrows():
+#         temp_rankings.append([rank, row["business_id"]])
+#         rank += 1
+#     temp_rankings = sorted(temp_rankings, reverse=True)
+#     return temp_rankings
 
-# imports
+
+
+
+
+
+
+
+
+
+
+
+######################################################################################################################
+
+
+
+
+
+# Ideas: At the moment takes a business id and returns a list of predictions (based only on categories which is poor)
+# What about: It sums the combined predictions for businesses the user has already reviewed?  And then orders the
+# Matching businesses from the cascade accordingly
+
+
+# Base code from:
+#   https://github.com/nikitaa30/Content-based-Recommender-System/blob/master/recommender_system.py
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-# Method
-# 1. Information Source
-# 2. Content Analyser
-# 3. Represented items -> (Profile Learner & Profiles) + (Filtering Component)
-# 4. List of recommendations
+ds = pd.read_csv("newDFBusiness.csv")
 
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(ds['categories'])
 
-def content_based_recommender(user_id):
-    # Update the dataframes
-    reviews_df = pd.read_csv("newDFReview.csv")
-    users_df = pd.read_csv("newDFUser.csv")
-    businesses_df = pd.read_csv("newDFBusiness.csv")
+cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-    return temporary_recommender(businesses_df)
+results = {}
 
-    # Content analyser - Represent items' content in a structured form
-    analysis = content_analyser(user_id, users_df, businesses_df, reviews_df)
+for idx, row in ds.iterrows():
+    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+    similar_items = [[cosine_similarities[idx][i], ds['business_id'][i]] for i in similar_indices]
 
-    # Profile learner - Collect and generalise user preference data and construct user profile
+    results[row['business_id']] = similar_items[1:]
 
-    # Filtering component - Apply user profile model to new item representations
-    #   Match user profile representation to item representations
-    #   Generate a prediction / relevance judgment / score
-    #   Present a (ranked) list of item recommendations
-
-
-def content_analyser(user_id, users_df, businesses_df, reviews_df):
-    return "hi"
-
-
-def temporary_recommender(businesses_df):
-    rank = 1
-    temp_rankings = []
-    for index, row in businesses_df.iterrows():
-        temp_rankings.append([rank, row["business_id"]])
-        rank += 1
-    temp_rankings = sorted(temp_rankings, reverse=True)
-    return temp_rankings
+print(results)
