1a5d2fb3c12bbe43c981c464e9ab8c3c0405d158
Kai <kwiddeson4@gmail.com>
2021-02-15 16:48:14 +0000

    light spring cleaning

--- MyCode/contentBasedRecommender.py
+++ MyCode/contentBasedRecommender.py
@@ -1,70 +1,26 @@
-# # This recommender uses content-based filtering
-#
-# # imports
-# import pandas as pd
-#
-#
-# def content_based_recommender(user_id):
-#     # Update the dataframes
-#     reviews_df = pd.read_csv("newDFReview.csv")
-#     users_df = pd.read_csv("newDFUser.csv")
-#     businesses_df = pd.read_csv("newDFBusiness.csv")
-#
-#     return temporary_recommender(businesses_df)
-#
-#
-# def temporary_recommender(businesses_df):
-#     rank = 1
-#     temp_rankings = []
-#     for index, row in businesses_df.iterrows():
-#         temp_rankings.append([rank, row["business_id"]])
-#         rank += 1
-#     temp_rankings = sorted(temp_rankings, reverse=True)
-#     return temp_rankings
+# This recommender uses content-based filtering
 
-
-
-
-
-
-
-
-
-
-
-
-######################################################################################################################
-
-
-
-
-
-# Ideas: At the moment takes a business id and returns a list of predictions (based only on categories which is poor)
-# What about: It sums the combined predictions for businesses the user has already reviewed?  And then orders the
-# Matching businesses from the cascade accordingly
-
-
-# Base code from:
-#   https://github.com/nikitaa30/Content-based-Recommender-System/blob/master/recommender_system.py
-import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-def content_based_recommender(reviewed_items, refined_businesses):
 
+def content_based_recommender(reviewed_items, refined_businesses):
+    similarity_measure = refined_businesses['categories']
     # TF-IDF algorithm, weights the importance of keywords based on frequency, higher weight = rarer and more important
     # TF = Term frequency, IDF = Inverse document frequency (relative significance)
     # Stop words are like "an" and "the" and hold no significant meaning so are removed
-    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
-    tfidf_matrix = tf.fit_transform(refined_businesses['categories'])
-
+    # An n-gram range of 3-14 has been chosen so that all categories can be included within a single n-gram, for example
+    #   both "pubs" and "sports bars"
+    #
     # Calculate the relevance of one item to another
+    # The following block of code (next 7 lines) have been adapted from:
+    #   https://github.com/nikitaa30/Content-based-Recommender-System/blob/master/recommender_system.py
+    tfidf_matrix = TfidfVectorizer(ngram_range=(3, 14), stop_words='english').fit_transform(similarity_measure)
     cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
     results = {}
     for index, row in refined_businesses.iterrows():
         similar_indices = cosine_similarities[index].argsort()[:-100:-1]
         similar_items = [[cosine_similarities[index][i], refined_businesses['business_id'][i]] for i in similar_indices]
-
         results[row['business_id']] = similar_items[1:]
 
     # Sum the similarities to all previously reviewed items to find most similar businesses to the user profile
